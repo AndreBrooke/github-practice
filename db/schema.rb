ce624015f3a53ac2c54148d7403bbaddddd3681e
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-10-03 11:37:26 +0800

    my part feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003030901) do
+ActiveRecord::Schema.define(version: 20171003033057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "location"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.boolean "check"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
